9708a75d808f462fafbd139754f5358fefd45ad7
Salman Faris <farissalmannbr@gmail.com>
2017-01-04 23:30:17 +0530

    Create telegrambot.py

---
+++ telegrambot.py
@@ -0,0 +1,38 @@
+#coder :- Salman Faris
+
+import sys
+import time
+import random
+import datetime
+import telepot
+import RPi.GPIO as GPIO
+
+#LED
+def on(pin):
+        GPIO.output(pin,GPIO.HIGH)
+        return
+def off(pin):
+        GPIO.output(pin,GPIO.LOW)
+        return
+# to use Raspberry Pi board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# set up GPIO output channel
+GPIO.setup(11, GPIO.OUT)
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print 'Got command: %s' % command
+
+    if command == 'on':
+       bot.sendMessage(chat_id, on(11))
+    elif command =='off':
+       bot.sendMessage(chat_id, off(11))
+
+bot = telepot.Bot('321449296:AAGw2eb8EK1ii9FkanAF5WQ3KLHWAljVKp0')
+bot.message_loop(handle)
+print 'I am listening...'
+
+while 1:
+     time.sleep(10)
