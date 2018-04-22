06e3654087d12697dbc8f7b04dd9fd58d287a3e2
Wing <puahhuiying@gmail.com>
2018-04-22 23:15:06 +0800

    Added try, except block

--- telegrambot.py
+++ telegrambot.py
@@ -2,8 +2,6 @@
 
 import sys
 import time
-import random
-import datetime
 import telepot
 import RPi.GPIO as GPIO
 
@@ -23,7 +21,7 @@ def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
 
-    print 'Got command: %s' % command
+    print('Got command: %s' % command)
 
     if command == 'on':
        bot.sendMessage(chat_id, on(11))
@@ -32,7 +30,17 @@ def handle(msg):
 
 bot = telepot.Bot('Bot Token')
 bot.message_loop(handle)
-print 'I am listening...'
+print('I am listening...')
 
 while 1:
-     time.sleep(10)
+    try:
+        time.sleep(10)
+    
+    except KeyboardInterrupt:
+        print('\n Program interrupted')
+        GPIO.cleanup()
+        exit()
+    
+    except:
+        print('Other error or exception occured!')
+        GPIO.cleanup()
